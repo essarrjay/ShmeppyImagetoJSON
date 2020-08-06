de8b9d5971bec2b40c4408d055b246b0a5cb9f0f
essarrjay <essarrjay@protonmail.com>
2020-08-06 12:55:14 -0700

    Removed unused method .export_json
    this method is now part of the Game_Map class in game_map.py

--- shmops.py
+++ shmops.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-import json
 from pathlib import Path
 
 export_obj = {"exportFormatVersion":1,"operations":[]}
@@ -14,23 +12,3 @@ class Fill_Operation:
         coordinates = [x_coord,y_coord]
         fill_data = [coordinates,color]
         self.cellFills.append(fill_data)
-
-    def export_json(self, data_dir=r'./output_files/'):
-        #exports table_data as JSON
-        if data_dir[-1] != '/': data_dir += '/'
-
-        #generate export path
-        timestamp_str = str(datetime.now())[:-7]
-        timestamp_str = timestamp_str.replace(':','').replace(' ','_')
-        filename = f'game_map_{timestamp_str}.json'
-        export_path = Path(data_dir + filename)
-        export_obj["operations"].append(self.__dict__)
-
-        try:
-            result = f"Exporting mapfile to {str(export_path)}"
-            with export_path.open(mode='w') as json_file:
-                json.dump(export_obj,json_file)
-        except:
-            result = "Error, unable to export."
-
-        return result
