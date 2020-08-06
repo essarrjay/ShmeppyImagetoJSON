dee02a91f26eb194e552215c0b7a8e8fd2a4f19e
essarrjay <essarrjay@protonmail.com>
2020-08-06 14:45:23 -0700

    progress_bar now accepts a suffix
    between bar and counter

--- progress_bar.py
+++ progress_bar.py
@@ -1,9 +1,11 @@
 import sys
 
-def progressbar(it, prefix="", width=60, file=sys.stdout):
+def progressbar(it, prefix="", suffix="", width=60, file=sys.stdout):
     """Usage:
-    for i in progressbar(range(15), "Computing: ", 40):
+    for i in progressbar(range(15), "Processing: ",  "Part ", 40):
         <some long running calculation>
+
+    Processing: [####################################] Part 16/16
     """
 
     count = len(it)
@@ -12,7 +14,7 @@ def progressbar(it, prefix="", width=60, file=sys.stdout):
         bar = "#"*x
         remaining = "."*(width-x)
         num = j
-        file.write(f"{prefix}[{bar}{remaining}]{num}/{count}\r")
+        file.write(f"{prefix} [{bar}{remaining}] {suffix}{num}/{count}\r")
         file.flush()
     show(0)
     for i, item in enumerate(it):
