b1fd74dc8ddcbd4ae9cdd5b49f18444386c68da0
essarrjay <essarrjay@protonmail.com>
2020-08-05 21:58:49 -0700

    New Menu Option partially implemented
    Needs additional testing, might have issues with certain tiles.

---
+++ menu.py
@@ -0,0 +1,50 @@
+from PyInquirer import prompt, Separator
+from pyfiglet import Figlet
+import sys
+
+global answers
+questions = [
+    {
+        'type': 'list',
+        'name': 'process_option',
+        'message': '-= How would you like to process this image? =-',
+        'choices': [
+            'Limited Palette',
+            'Blended (BOX filter)',
+            Separator(),
+            'Learn MOre'
+        ],
+        'default': 'Limited Palette',
+        'filter': lambda val: val.lower()
+    },
+    {
+        'type': 'input',
+        'name': 'max_map_dim',
+        'message': '-= Maximum map dimension in squares: ',
+        'filter': lambda val: int(val)
+    },
+    {
+       'type': 'input',
+        'name': 'palette_size',
+        'message': '-= Palette Size for the Map Image in # of Colors: ',
+        'filter': lambda val: int(val),
+        'when': lambda answers: answers['process_option'] == 'limited palette'}
+]
+
+image_file_question = {
+    'type': 'input',
+    'name': 'img_path',
+    'message': '-= Image File Path/Name: '}
+
+def check_for_filename():
+    global questions
+    questions = [image_file_question, *questions]
+
+def main(have_file=False):
+    if not have_file: check_for_filename()
+    answers = prompt(questions)
+    #print(answers)
+    return(answers)
+
+if __name__ == '__main__':
+    main()
--- shmeppify.py
+++ shmeppify.py
@@ -7,6 +7,7 @@ from pyfiglet import Figlet
 
 #internal modules
 import process_image
+import menu
 
 
 def get_max_dim_input():
@@ -19,19 +20,6 @@ def get_max_dim_input():
             print("Sorry, let's try that again.\n")
     return dim
 
-def get_filter_option():
-    option = -1
-    filters_map = {1:Image.BOX,2:Image.NEAREST} #uses Image resampling options
-    while option <= 0:
-        print("-= Filter Options =-")
-        print("1. BOX (smooth, but some blur)\n2. NEAREST (crisp, artifacts)\n")
-        try:
-            option = int(input("Please select a filter option by number (default BOX): ") or '1')
-            output = filters_map[option]
-        except:
-            print("Sorry, please select a option from the menu.\n")
-    return output
-
 def get_palette_size_input():
         ps = -1
         print("-= Palette Size for the Map Image in # of Colors =-")
@@ -42,7 +30,32 @@ def get_palette_size_input():
                 print("Sorry, let's try that again.\n")
         return ps
 
-def run():
+def get_process_option():
+    option = -1
+    filters_map = {2:Image.BOX,3:Image.NEAREST} #uses Image resampling options
+    while option <= 0 or option > 2:
+        print("-= Process Options =-")
+        print("1. Limited Palette\n2. Blended (BOX filter)")
+        try:
+            option = int(input("Please select a filter option by number (default BOX): ") or '1')
+            output = filters_map[option]
+        except:
+            print("Sorry, please select a option from the menu.\n")
+    return output
+
+def process_img_depreciated(image_path,max_map_dim, filter_option):
+    #mapsize as a tuple
+    map_img = Image.open(image_path)
+    map_img.thumbnail((max_map_dim,max_map_dim),resample=filter_option)
+    pixels = map_img.convert('RGB').load()
+    output_op = Fill_Operation(id='4321')
+    for x in range(map_img.width):
+        for y in range(map_img.height):
+            r,g,b = pixels[x,y]
+            output_op.add_pixel(x,y,rgb_to_hex(r,g,b))
+    print(output_op.export_json())
+
+def run_depreciated():
     f = Figlet(font='slant')
     print(f.renderText('Shemppy Image\nto JSON'))
 
@@ -51,15 +64,32 @@ def run():
     except:
         img_path = input("Image File Path/Name: ")
 
+    process_option = get_process_option()
+
     max_map_dim = get_max_dim_input()
     print()
     palette_size = get_palette_size_input()
     print()
-    #filter_option = get_filter_option()
+
     fill_op = process_image.image_to_operation(img_path, max_map_dim, palette_size)
     print()
     print(fill_op.export_json())
 
+def run():
+    f = Figlet(font='slant')
+    print(f.renderText('Shemppy Image\nto JSON'))
+    answers = {}
+    try:
+        img_path = sys.argv[1]
+        answers['img_path'] = img_path
+    except:
+        img_path = False
+    answers.update(menu.main(img_path))
+    process_option = answers.pop('process_option')
+    print(answers)
+    fill_op = process_image.image_to_operation(**answers)
+    print()
+    print(fill_op.export_json())
 
 if __name__ == '__main__':
     run()
