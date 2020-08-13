a88f18afffb5c8efe3a13134b768a198c827fe0e
essarrjay <essarrjay@protonmail.com>
2020-08-12 20:40:50 -0700

    resize option for palette ops
    speeds up palette operations by resizing image prior to all the 
    (inefficient) palette interations.

--- shmeppify.py
+++ shmeppify.py
@@ -25,20 +25,39 @@ def main(title=True):
     if answers['debug']: print(answers)
     try:
         map_size = (answers['map_dim_x'],answers['map_dim_y'])
+        map_major_dim = max(map_size)
     except:
+        map_major_dim = answers['map_major_dim']
         map_size = (answers['map_major_dim'])
-    gm = Game_Map(answers['img_path'],map_size,answers['debug'])
 
     if answers['op_type'].startswith('pal'):
+        #process palette operation
+        if answers['palette_rescale']:
+            #resize image before processing
+            in_path = Path(answers['img_path'])
+            temp_path = Path('resized_'+in_path.name)
+            with Image.open(in_path) as im:
+                size = min(10*map_major_dim,*im.size)
+                im.thumbnail((size,size),resample=Image.LANCZOS)
+                im.save(temp_path)
+            answers['img_path'] = str(temp_path)
+
+        #create Game_Map and Fill_Operation
+        gm = Game_Map(answers['img_path'],map_size,answers['debug'])
         op = gm.palette_op(answers['palette_size'], sample_factor = answers['sample_factor'])
+
+        #remove palette_rescale file if present
+        temp_path.unlink(missing_ok=True)
+
     elif answers['op_type'].startswith('fil'):
+        gm = Game_Map(answers['img_path'],map_size,answers['debug'])
         op = gm.filter_op(answers['filter_type'])
     else:
         print("Sorry, something went wrong.")
 
+    #Export to Shmeppy map as a JSON file
     result = gm.op_to_json(op)
     print(result)
-
 
 if __name__ == '__main__':
     main()
