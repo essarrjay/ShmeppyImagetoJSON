08f7c9ac8fd4ef5aa3d34f826c430ee75e4e5a0e
essarrjay <essarrjay@protonmail.com>
2020-08-15 16:13:30 -0700

    spaced prompt lines
    and cleaned comments

--- menu.py
+++ menu.py
@@ -7,25 +7,29 @@ def main_menu(img_path=None):
     print("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
     print("  Image to Shmeppy JSON Converter ")
     print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+    #initialize answers
     answers={'img_path':img_path, 'map_dim_y':None}
+
+    #check for image path
     if not img_path:
         answers['img_path'] = input("-= Image File Path/Name: ")
+
     # Get the op_type
     print("-= How would you like to process this image? =-")
+
     # List of op_types to select from, including some captions
     op_types = ['Palette - sharp tiles, slow',
         'Filter resize - faster, blended tiles',
         '------',
         'Help: Learn More',
         'Exit']
-    # List entries which are captions and thus not selectable
-    captions = [2]
     answers['op_type'] = op_types[
-        cutie.select(op_types, caption_indices=captions,caption_prefix="   ")].lower()
+        cutie.select(op_types, caption_indices=[2],caption_prefix="   ")].lower()
 
+    #check for early exits
     if answers['op_type'].startswith('exit'):
         exit()
-
     elif answers['op_type'].startswith('help'):
         p = Path("help.txt")
         with open(p, encoding="utf8") as ht:
@@ -35,8 +39,12 @@ def main_menu(img_path=None):
     print()
     #prompt map dimensions or fetch major dimension
     answers['autoscale'] = cutie.prompt_yes_or_no('Autoscale with the map\'s major dimension?',yes_text="Yes, autoscale", no_text="No, I will provide two dimensions", default_is_yes=True)
+
+    print()
     answers.update(map_size_menu(answers['autoscale']))
 
+    print()
+    #prompt process type questions
     if answers['op_type'].startswith('fil'):
         answers.update(filter_menu())
 
@@ -46,9 +54,10 @@ def main_menu(img_path=None):
     print()
     answers['debug'] = cutie.prompt_yes_or_no('Debug Mode? ')
 
+    print()
+    #confirm to proceed
     answers['confirm'] = cutie.prompt_yes_or_no('Proceed with Processing? ', default_is_yes=True)
-
-    if not answers['confirm']: 
+    if not answers['confirm']:
         print("Processing terminated, exiting program.")
         exit()
 
@@ -78,7 +87,7 @@ def map_size_menu(autoscale=True):
 
 def filter_menu():
     response = {}
-    print('\n-= Which filter would you like to use? =-')
+    print('-= Which filter would you like to use? =-')
     choices = ['NEAREST - sharp tiles, some artifacts','BOX - idential weight','BILINEAR - linear interpolation','HAMMING - sharper than BILINEAR, less distortion than BOX','BICUBIC - cubic interpolation','LANCZOS - trucated sinc']
     val = choices[cutie.select(choices,selected_index=1)].lower()
     response['filter_type'] = lookup_filter(val)
@@ -87,7 +96,6 @@ def filter_menu():
 
 def palette_menu():
     response = {}
-    print()
 
     response['sample_factor'] = cutie.get_number(
         '-= Sample grid major dimension (integer):',
@@ -95,12 +103,14 @@ def palette_menu():
         max_value=999,
         allow_float=False)
 
+    print()
     response['palette_size'] = cutie.get_number(
         '-= Number of Palette Colors to Return per Sample (8 max, enter 0 for automatic selection):',
         min_value=0,
         max_value=8,
         allow_float=False)
 
+    print()
     response['palette_rescale'] = cutie.prompt_yes_or_no('-= Rescale Image before palette sample (reduces processing time)?',yes_text="Yes, rescale", no_text="No, use original image dimensions", default_is_yes=True)
 
     return response
