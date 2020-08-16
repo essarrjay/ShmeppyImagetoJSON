8d3ea28f67af00582b05e1e970f306c8f2673ccb
essarrjay <essarrjay@protonmail.com>
2020-08-16 11:17:12 -0700

    updated docstrings

--- palette.py
+++ palette.py
@@ -2,10 +2,10 @@ from haishoku.haishoku import Haishoku
 from sys import exit
 
 def get_palette(img_path, palette_size, debug=False, show_palette=False, autothreshold = None):
-    #returns a list of RGB tuples representing a palette of palette_size numbers of color, by maximum use
     if palette_size == 0:
         palette_size = 8
         autothreshold = 1 / palette_size
+    """returns a list of RGB tuples representing a palette of palette_size numbers of color, by maximum use"""
 
     try:
         full_palette = Haishoku.getPalette(str(img_path))
@@ -41,7 +41,7 @@ def get_palette(img_path, palette_size, debug=False, show_palette=False, autothr
     return output
 
 def nearest_color_from_palette(palette, in_color):
-    #returns nearest RGB color in palette
+    """returns nearest RGB color in palette"""
     out_color = min( palette, key = lambda palette_entry: sum((pe - c) ** 2 for pe, c in zip(palette_entry, in_color)))
     return out_color
 
