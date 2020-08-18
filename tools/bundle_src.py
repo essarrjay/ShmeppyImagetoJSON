7edffe454f65402bfdd46a398e843030b0702f2e
essarrjay <essarrjay@protonmail.com>
2020-08-18 10:13:16 -0700

    fixed bundling issue
    and converts README.md to html

--- tools/bundle_src.py
+++ tools/bundle_src.py
@@ -1,16 +1,51 @@
 import tarfile
 from pathlib import Path
+import markdown
+from os import remove
 
-ersion = "2.2.1"
-BASE_DIR = Path(__file__).parent.parent
-out_path = BASE_DIR.joinpath('build', f'img_to_json_python_bundle_v{ersion}.tar')
+ERSION = input("Version Number: ")
+BASE_DIR = Path(__file__).resolve().parent.parent
+out_path = BASE_DIR.joinpath('build', f'img_to_json_python_bundle_v{ERSION}.tar')
 in_path = BASE_DIR.joinpath('src')
 
 print(f'Base Dir: {BASE_DIR}')
 print(f'Out_path: {out_path}')
 print(f'In_path: {in_path}')
 
-with tarfile.open(out_path, "w") as tar:
-    tar.add(Path("../README.md"))
-    for f in in_path.iterdir():
-        if f.is_file(): tar.add(f)
+def reset(tarinfo):
+    tarinfo.uid = tarinfo.gid = 0
+    tarinfo.uname = tarinfo.gname = ""
+    return tarinfo
+
+def md_to_html_file(p):
+    """pathlib.Path markdown file to html file"""
+    with open(p, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+    html = markdown.markdown(text)
+
+    out_path = p.resolve().parent.joinpath(p.stem + ".html")
+
+    with open(out_path, "w", encoding="utf-8", errors="xmlcharrefreplace") as out_file:
+        out_file.write(html)
+
+    return out_path
+
+def main():
+    """Bundles files needed to run app in Python"""
+    readme_html = md_to_html_file(BASE_DIR.joinpath("README.md"))
+
+    with tarfile.open(out_path, "w") as tar:
+        tar.add(readme_html,arcname=readme_html.name)
+        for f in in_path.iterdir():
+            print(f'Checking File: {"/".join(f.parts[-3:])}')
+            if not f.is_dir():
+                fin = Path(*f.parts[-2:])
+                print(f'Adding file as: {fin}\n')
+                tar.add(f, arcname=fin, recursive="False", filter=reset)
+            else:
+                print("Directory excluded.\n")
+    remove(readme_html)
+    return print(f'Files bundled to: {out_path}')
+
+if __name__ == '__main__':
+    main()
