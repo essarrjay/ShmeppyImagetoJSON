f4f7370230c9368083baf274c3642073fbeaf823
essarrjay <essarrjay@protonmail.com>
2020-08-19 22:11:16 -0700

    fixed an issue with finding import map

--- tools/build_combiner.bat
+++ tools/build_combiner.bat
@@ -2,7 +2,7 @@ pyinstaller ^
 --onefile ^
 --console ^
 --noconfirm ^
---name map_combiner_v0.2.1 ^
+--name map_combiner_v0.2.2 ^
 --distpath ../build ^
 --workpath ../pyinstaller_workfiles ^
 --specpath ../pyinstaller_workfiles ^
