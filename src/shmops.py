6880fd972576866ffc2e3233d35f778453f5bd6b
essarrjay <essarrjay@protonmail.com>
2020-08-20 20:46:32 -0700

    framed UpdateCellEdges op

--- src/shmops.py
+++ src/shmops.py
@@ -19,3 +19,10 @@ class Fill_Operation(Shm_Op):
         coordinates = [x_coord,y_coord]
         fill_data = [coordinates,color]
         self.cellFills.append(fill_data)
+
+class UpdateCellEdges(Shm_Op):
+    """Represents a single UpdateCellEdges operation"""
+    def __init__(self, id='', type='UpdateCellEdges'):
+        Shm_Op.__init__(self,id,type)
+        self.left = []
+        self.top = []
