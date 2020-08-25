adcfa20caffc9b63079a7faad36b0a1a1c3ac151
essarrjay <essarrjay@protonmail.com>
2020-08-24 21:32:36 -0700

    fixed bug with tokens resized from upper left corner

--- shmeppytools/mapcombiner/token.py
+++ shmeppytools/mapcombiner/token.py
@@ -1,17 +1,17 @@
 
 class Token:
     """represents a single shmeppy token"""
-    def __init__(self, id, type, tokenId, position=(0, 0), color="#FFF", width=1, height=1):
+    def __init__(self, id, type, tokenId, position=(0, 0), color="#FFF", label="", width=1, height=1):
         self.id = tokenId
-        self.tokenid = tokenId
+        self.tokenId = tokenId
         self.type = 'CreateToken'
         self.position = position
         self.color = color
-        self.label = ""
+        self.label = label
         self.width = width
         self.height = height
 
-    def update(self, id=None, type=None, position=None, color=None, label="", tokenId=None, width=None, height=None):
+    def update(self, id=None, type=None, tokenId=None, position=None, color=None, label="", width=None, height=None, x=None, y=None):
         """updates token properties with any value present"""
         print(f' UPDATE INFO = P:{position}, C:{color}, L:{label}, W:{width}, H:{height}')
         if position:
@@ -24,6 +24,10 @@ class Token:
             self.width = width
         if height:
             self.height = height
+        if x:
+            self.position[0] += x
+        if y:
+            self.position[1] += y
 
     def as_op(self):
         """exports the token as a shmeppy compatible operation"""
