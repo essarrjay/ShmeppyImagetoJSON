e7886c9841db3af676da05a92e6f6c14779d2960
essarrjay <essarrjay@protonmail.com>
2020-08-26 21:01:15 -0700

    combine_maps now uses Shmap.py

--- shmeppytools/mapcombiner/combine_maps.py
+++ shmeppytools/mapcombiner/combine_maps.py
@@ -1,41 +1,20 @@
 #! usr/bin/env python3
 
+# external modules
 import json
 from pathlib import Path
 from collections import Counter
-from copy import deepcopy
 import sys
 from datetime import datetime
 
+# internal modules
+from shmap import Shmap
+
 PADDING = 10
-BASE_PATH = Path(__file__).resolve().parent.parent
-#SAME_PATH = Path(__file__).resolve().parent
+BASE_PATH = Path(__file__).resolve().parent
 CELL_OPS = {'FillCells': ['cellFills'], 'UpdateCellEdges': ["top", "left"]}
 OTHER_OPS = {'CreateToken': ['color', 'position']}
 SHMEP_DICT = {"exportFormatVersion": 1, "operations": []}
-
-
-def get_map_bounding_box(map):
-    """returns bounding box of a map in squares x,y"""
-    print(" --------------------")
-    print(" Getting Bounding Box")
-    print(" --------------------")
-    x_list, y_list = [], []
-
-    for op in map['operations']:
-        print(f"op_type is {op['type']}")
-        if op['type'] in CELL_OPS.keys():
-            for action in CELL_OPS[op['type']]:
-                if op['type'] in ["FillCells", "UpdateCellEdges"]:
-                    xs, ys = fillcells_xys(op[action])
-                #TODO: add other op types
-                    x_list += xs
-                    y_list += ys
-        else:
-            print(f"{op['type']} is NOT drawing op")
-    ul_corner = min(x_list), min(y_list)
-    lr_corner = max(x_list), max(y_list)
-    return ul_corner, lr_corner
 
 
 def fillcells_xys(action):
@@ -59,12 +38,6 @@ def get_new_corners(bb1, bb2):
     return ul_corner, lr_corner
 
 
-def get_dimensions(bb):
-    """returns absolute dimensions of bounding box"""
-    (ux, uy), (lx, ly) = bb
-    return lx-ux+1, ly-uy+1
-
-
 def count_ops(map):
     print("................")
     print("Operations Count")
@@ -76,65 +49,43 @@ def count_ops(map):
         print(f'{k} : {v}')
 
 
-def translate_op(op, offset, new_id):
-    """translate op by offset value"""
-    for action in CELL_OPS[op['type']]:
-        cell_list = []
-        for cell in op[action]:
-            x = cell[0][0]+offset[0]
-            y = cell[0][1]+offset[1]
-            cell_list.append([[x, y], cell[1]])
-        op[action] = cell_list
-    op["id"] = new_id
-    return op
-
-
-def get_updated_ops(map, offset):
-    """for map, offset all draw operations"""
-    outops = []
-    for op in map['operations']:
-        if op['type'] in CELL_OPS.keys():
-            #print(f"translating {op['type']} by the following dimensions: {offset}")
-            outops.append(translate_op(op, offset, new_id='1234'))
-        elif op['type'] in OTHER_OPS:
-            pass
-    return outops
-
-
 def combine_maps(map_list, layout=(0, 1), padding=0):
     """combine maps in map_list with padding"""
     total_x, total_y = 0, 0
     h, v = layout
     layout = {}
     for map in map_list:
-        bb = get_map_bounding_box(map)
+        bb = map.set_bounding_box()
         print(f"bounding boxes = {bb}")
-        md = get_dimensions(bb)
-        print(f"map dimensions = {md}")
+        mapdim = map.get_bb_dimensions()
+        print(f"map dimensions = {mapdim}")
 
         # create relative placement of map
         x = total_x-bb[0][0]
         y = total_y-bb[0][1]
 
-        layout.update({(x, y): (map, md)})
+        layout.update({(x, y): (map, mapdim)})
 
         # set next map's coordinates
-        total_x += (md[0]+padding)*h
-        total_y += (md[1]+padding)*v
+        total_x += (mapdim[0]+padding)*h
+        total_y += (mapdim[1]+padding)*v
 
     print(" +++++++++++++++++++++++++++++++")
     print("  Writing New Drawing Operations")
     print(" +++++++++++++++++++++++++++++++")
 
-    # translate maps
-    outmap = deepcopy(SHMEP_DICT)
-    i = 1
-    for offset, (map, md) in layout.items():
-        print(f"Map #{i} of size {md[0]}x{md[1]} being offset by x,y={offset}.")
-        outmap['operations'] += get_updated_ops(map, offset)
-        i += 1
-
+    # offset maps
+    outmap = Shmap('outmap')
+    print(f"shmap ops: {outmap.operations}")
+    for offset, (map, mapdim) in layout.items():
+        print(f"Map {map.name} of size {mapdim[0]}x{mapdim[1]} being offset by x,y={offset}.")
+        map.offset_ops(offset)
+        outmap.operations += map.as_ops()
     return outmap
+
+
+def path_to_Shmap(inpath):
+    return Shmap(inpath.name, import_map(inpath))
 
 
 def import_map(inpath):
@@ -170,48 +121,55 @@ def main():
     print("    OR")
     print(" > python combine_maps.py <map-1 path> <map-2 path>...<map-n path>\n")
 
-    # maps from command line
     try:
+        # getting maps from command line
         if len(sys.argv) == 1:
             raise Exception("Did not find maps. Please provide below.")
         map_list = []
         for p in sys.argv[1:]:
             print(f"Provide Map Path: {p}")
-            temp_p = BASE_PATH.joinpath(p)
-            print(f"Attempting to import map from: {temp_p}")
-            map_list.append(import_map(temp_p))
+            temp_p = Path.cwd().joinpath(p)
+            print(f"Attempting to import map from: {temp_p.resolve()}")
+            map_list.append(path_to_Shmap(temp_p))
     except Exception as e:
+        # prompt for maps instead
         print(e)
-        print(f"Looking for maps in: {BASE_PATH.resolve()}")
+        print(f"Looking for maps in: {Path.cwd().resolve()}")
         print("If maps are in this folder, just list mapname including")
         print("file extension (.json) otherwise include the folder name")
         print("E.g. mymap.json or backup_maps/mymap.json")
-        mpath1 = input("Please provide relative path to map #1: ")
-        mpath2 = input("Please provide relative path to map #2: ")
+        mapstr1 = input("Please provide relative path to map #1: ")
+        mapstr2 = input("Please provide relative path to map #2: ")
+        mpath1 = Path.cwd().joinpath(mapstr1)
+        mpath2 = Path.cwd().joinpath(mapstr2)
 
-        # import maps
+        # try importing maps
         print("Loading Mapfiles:")
         try:
-            map_list = [import_map(BASE_PATH.joinpath(mpath1)), import_map(BASE_PATH.joinpath(mpath2))]
+            map_list = [path_to_Shmap(mpath1), path_to_Shmap(mpath2)]
         except Exception:
-            print(f"tried: {BASE_PATH.joinpath(mpath1)}\n{BASE_PATH.joinpath(mpath2)}")
+            # try again
+            print(f"tried: {mpath1}\n{mpath2}")
             print("\n\nERROR: File not found, let's try again (or press ctrl+c to quit)\n\n")
             return main()
 
     pad = input(f"Minimum PADDING between maps (in squares). Or press enter for default value of {PADDING}: ")
     pad = int(pad) if pad else PADDING
 
-    print(f"\nOutput destination currently set to:\n {BASE_PATH}")
+    print(f"\nOutput destination currently set to:\n {Path.cwd()}")
     outdest = input("Enter to continue, or enter full path to set output destination: ")
-    outdest = Path(outdest) if outdest else BASE_PATH
+    outdest = Path(outdest) if outdest else Path.cwd()
 
+    # make new map
     new_map = combine_maps(map_list, padding=pad)
 
+    # make filename
     ts = str(datetime.now())[:-7]
     ts = ts.replace(':', '').replace('-', '').replace(' ', '_')
     filename = f"combined_map_{ts}.json"
 
-    print(export_map(new_map, outdest.joinpath(filename)))
+    # export and print result
+    print(export_map(new_map.get_json(), outdest.joinpath(filename)))
 
     # pause before exiting - necessary for pyinstaller
     input("Press Enter to Exit...")
